3e3901ee06e06ec21c98e23f3fc0762efde82c15
abhishekjainvs <abhishekjain.vs@gmail.com>
2017-04-30 13:34:46 +0530

    final 1

---
+++ rahultest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="one">hello</div>
+<button onclick="fn()">click</button>
+<script type="text/javascript">
+	function fn()
+	{
+		document.getElementsByClassName("one").style.display = "none";
+	}
+</script>
+</body>
+</html>
